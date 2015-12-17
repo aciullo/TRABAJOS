ca4bceccfc18cbdccdc2e67686ec71e08f4f021f
aciullo <andrea.ciullo@hotmail.com>
2015-12-17 17:28:54 -0300

    171215

--- resumen cobros.sql
+++ resumen cobros.sql
@@ -1,4 +1,4 @@
-declare @idempresa char(3)
+declare @idempresa char(3), @dfecha date, @hfecha date,@sucursal char(3)
 set @idempresa='050'
 
 
@@ -10,7 +10,8 @@ select a.idpago
 ,rtrim(a.idcliente)+'-'+c.razsocial cliente
 ,	rtrim(a.idcobrador)+'-'+rtrim(e.nombre)+' '+rtrim(e.apellido) cobrador
 ,c.Ruc
-,	VD.numero,VD.idmoneda,VD.importe,VD.importe_pag,f.fecha fechafact,TotalFacturas,f.Idcomprobante,VD.Cuota	
+,VD.numero
+,VD.idmoneda,VD.importe,VD.importe_pag,f.fecha fechafact,TotalFacturas,f.Idcomprobante,VD.Cuota	
 ,v.nrocheque, v.fchcheque, v.importe, b.descripcion AS Banco, tv.tipovalor,IdCobro,S.Descripción sucursal 
 from vt_pagos a 
 inner join vt_det_pagos VD on a.idpago=VD.idpago and a.idempresa=@idempresa	
@@ -25,11 +26,14 @@ left join vt_factura f on VD.idfactura=f.idfactura
 where 	 ( (A.fecha between @dfecha and @hfecha) or  @dfecha IS NULL OR  @hfecha IS NULL ) and (S.sucursal=@sucursal or @sucursal is null) 
 
 
-	SELECT     v.nrocheque, v.fchcheque, v.importe, b.descripcion AS Banco, tv.tipovalor,IdCobro
-FROM         ts_valores_base AS v LEFT OUTER JOIN
-                      bs_bancos AS b ON v.idbanco = b.idbanco LEFT OUTER JOIN
-                      ts_tipovalor AS tv ON v.idtipovalor = tv.idtipovalor
-                      where v.IdCobro= ?m.IdRecibo
+--	SELECT     v.nrocheque, v.fchcheque, v.importe, b.descripcion AS Banco, tv.tipovalor,IdCobro
+--FROM         ts_valores_base AS v LEFT OUTER JOIN
+--                      bs_bancos AS b ON v.idbanco = b.idbanco LEFT OUTER JOIN
+--                      ts_tipovalor AS tv ON v.idtipovalor = tv.idtipovalor
+--                      where v.IdCobro= ?m.IdRecibo
 
 
-					  select * from vt_clientes where IdCliente='0001'
+--					  select * from vt_clientes where IdCliente='0001'
+SELECT * FROM vt_det_pagos
+
+select * from bs_Monedas
